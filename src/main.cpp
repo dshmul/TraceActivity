c8905260b2b4e4ee0951d24336107f132a4c89df
Andrew Briceno <andrewbriceno2000@gmail.com>
2022-09-15 14:20:36 -0400

    Create basic esp32 web server

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,25 @@
 #include "esp_wifi_types.h"
 #include "esp_wifi.h"
 #include "esp_err.h"
+#include "WiFi.h"
+#include "AsyncTCP.h"
+#include "ESPAsyncWebServer.h"
 
 #define PROBE_REQ_SUBTYPE 0b0100
 
 // recycled
 #define WIFI_CHANNEL_SWITCH_INTERVAL  (500)
 #define WIFI_CHANNEL_MAX               (11) // US has 11 channels
- 
+
+const char* ssid  = "SSID"; // replace with actual ssid
+const char* password = "PASSWORD"; // replace with actual password
+
+AsyncWebServer server(80);
+
+String chan = "";
+String rssi = "";
+String mac = "";
+
 uint8_t channel = 1;
 
 // recycled (https://blog.podkalicki.com/esp32-wifi-sniffer/)
@@ -31,6 +43,8 @@ static void wifi_init();
 static void processMetadata(wifi_promiscuous_pkt_t *packet);
 static void wifi_packet_handler(void *packet_buff, wifi_promiscuous_pkt_type_t packet_type); // wifi_promiscuous_cb_t params
 static void wifi_set_channel(uint8_t wifi_channel);
+static void server_init();
+static void wifi_connect();
 
 void wifi_init()
 {
@@ -45,7 +59,9 @@ void wifi_init()
 
 void wifi_set_channel(uint8_t channel)
 {
+    WiFi.disconnect();
     ESP_ERROR_CHECK(esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE));
+    WiFi.reconnect();
 }
 
 void wifi_packet_handler(void *packet_buff, wifi_promiscuous_pkt_type_t packet_type)
@@ -76,13 +92,134 @@ static void processMetadata(wifi_promiscuous_pkt_t *packet)
         header->addr2[0],header->addr2[1],header->addr2[2],
         header->addr2[3],header->addr2[4],header->addr2[5]
     );
+
+    chan = String(packet->rx_ctrl.channel);
+    rssi = String(packet->rx_ctrl.rssi);
+    mac = String(header->addr2[0], 16) + ":" + String(header->addr2[1], 16) + ":"
+        + String(header->addr2[2], 16) + ":" + String(header->addr2[3], 16) + ":"
+        + String(header->addr2[4], 16) + ":" + String(header->addr2[5], 16);
+}
+
+const char index_html[] = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+  </style>
+</head>
+<body>
+  <h2>ESP32 Web Server</h2>
+  <p>
+    <span class="labels">Channel</span> 
+    <span id="channel">%CHANNEL%</span>
+  </p>
+  <p>
+    <span class="labels">RSSI</span>
+    <span id="rssi">%RSSI%</span>
+  </p>
+  <p>
+    <span class="labels">Mac Address</span> 
+    <span id="mac-address">%MAC_ADDRESS%</span>
+  </p>
+</body>
+<script>
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("channel").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/channel", true);
+  xhttp.send();
+}, 10 ) ;
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("rssi").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/rssi", true);
+  xhttp.send();
+}, 10 ) ;
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("mac-address").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/mac-address", true);
+  xhttp.send();
+}, 10) ;
+
+</script>
+</html>)rawliteral";
+
+String processor(const String& var){
+    //Serial.println(var);
+    if(var == "CHANNEL"){
+        return chan;
+    }
+    else if(var == "RSSI"){
+        return rssi;
+    }
+    else if(var == "MACADDRESS"){
+        return mac;
+    }
+    return String();
+}
+
+static void server_init(){
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.println("Connecting to WiFi..");
+        delay(1000);
+    }
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send_P(200, "text/html", index_html, processor);
+    });
+    server.on("/channel", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send_P(200, "text/plain", chan.c_str());
+    });
+    server.on("/rssi", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send_P(200, "text/plain", rssi.c_str());
+    });
+    server.on("/mac-address", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send_P(200, "text/plain", mac.c_str());
+    });
+    server.onNotFound([](AsyncWebServerRequest *request){
+        request->send(404, "text/plain", "Not found");
+    });
+    server.begin();
 }
 
 void setup()
 {
     Serial.begin(115200);
     delay(10);
-    
+    server_init();
     wifi_init();
 }
 
